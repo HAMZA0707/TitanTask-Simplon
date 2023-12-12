172738fa8b39c38945a5de1524e473b271708df9
hamza <hamzabenmoussa166@gmail.com>
2023-12-12 10:36:49 +0100

    Hamza first commit

--- titantask/src/main/java/titantask/Testclass.java
+++ titantask/src/main/java/titantask/Testclass.java
@@ -5,6 +5,7 @@ public class Testclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello World");
+		System.out.println("ana Hamza");
 	}
 
 }
