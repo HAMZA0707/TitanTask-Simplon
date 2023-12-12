722a329d4564fe84507f01668fc959710986171f
Adil errai <adilerrai24@gmail.com>
2023-12-12 12:49:08 +0100

    my first commit

---
+++ titantask/src/main/java/titantask/Utilisateur.java
@@ -0,0 +1,64 @@
+package titantask;
+
+
+public class Utilisateur {
+
+    private String nom;
+    private String fonction;
+    private String email;
+    private String password;
+    private int id ;
+
+    public Utilisateur(String nom, String fonction, String email, String password, int id) {
+        this.nom = nom;
+        this.fonction = fonction;
+        this.email = email;
+        this.password = password;
+        this.id = id;
+    }
+
+    public Utilisateur() {
+        super();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getFonction() {
+        return fonction;
+    }
+
+    public void setFonction(String fonction) {
+        this.fonction = fonction;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
