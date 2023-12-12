69dc24296dd71a5485c124e12dbad0c4c4570354
YassinXXXX03 <122600366+YassinXXXX03@users.noreply.github.com>
2023-12-12 10:53:09 +0100

    Yaasin commit

--- titantask/src/main/java/titantask/Testclass.java
+++ titantask/src/main/java/titantask/Testclass.java
@@ -5,12 +5,10 @@ public class Testclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello World");
-<<<<<<< HEAD
 		System.out.println("\nMariam Laghfiri");
-=======
 		System.out.println("ana Hamza");
 		System.out.println("Adil errai");
 		System.out.println("Talaini");
->>>>>>> 9c9aa905166d71c4751b15f444ae2adf3aeb3908
+		System.out.println("Ezaouibi Yassin");
 	}
 }
